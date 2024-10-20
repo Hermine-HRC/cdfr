8ef343e4b295fb52d4fc99bcd6b4f5267a20ffec
Axel Tremaudant <77966063+Axel927@users.noreply.github.com>
2024-10-20 18:49:57 +0200

    add shell functions wrapping some ros publisher

---
+++ scripts/README.md
@@ -0,0 +1,11 @@
+# scripts
+
+This folder contains useful scripts that can be used to facilitate some actions.
+
+# Scripts to source
+
+Some scripts contain shell functions so they need to be sourced to use these functions.
+
+Here is the list of file to source:
+
+* ros_functions.sh
---
+++ scripts/ros_functions.sh
@@ -0,0 +1,55 @@
+# FILE TO SOURCE
+
+# Publish on topic '/initial_pose' to set the current position of the robot manually
+# Takes as arguments: x in m, y in m
+set_pose () {
+    if [ "$#" -ne 2 ];
+	then
+		echo "You must give 'x' and 'y' as arguments"
+		return 1
+	fi
+
+	x=$1
+	y=$2
+	yaw=0
+
+	ros2 topic pub --once /initialpose geometry_msgs/PoseWithCovarianceStamped "{
+        header: {frame_id: 'map'},
+        pose: {
+            pose: {
+                position: { x: $x, y: $y, z: 0.0 },
+                orientation: { z: $(echo "s($yaw / 2)" | bc -l), w: $(echo "c($yaw / 2)" | bc -l) }
+            },
+            covariance: [
+                0.001, 0, 0, 0, 0, 0,
+                0, 0.001, 0, 0, 0, 0,
+                0, 0, 0.001, 0, 0, 0,
+                0, 0, 0, 0.001, 0, 0,
+                0, 0, 0, 0, 0.001, 0,
+                0, 0, 0, 0, 0, 0.001
+            ]
+        }
+    }"
+}
+
+# Publish on topic '/goal_pose' to send a goal position to the robot
+# Takes as arguments: x in m, y in m and yaw in rad
+goto () {
+    if [ "$#" -ne 3 ];
+	then
+		echo "You must give 'x', 'y' and 'yaw' as arguments"
+		return 1
+	fi
+
+	x=$1
+	y=$2
+	yaw=$3
+
+	ros2 topic pub --once /goal_pose geometry_msgs/PoseStamped "{
+        header: {stamp: {sec: 0}, frame_id: 'map'},
+        pose: {
+            position: {x: $x, y: $y, z: 0.0},
+            orientation: { z: $(echo "s($yaw / 2)" | bc -l), w: $(echo "c($yaw / 2)" | bc -l) }
+        }
+    }"
+}
