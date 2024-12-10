de6f65a00710c2a18e5dbfcbf78c9879fd2f7e9b
Axel Tremaudant <axelt.hrc@gmail.com>
2024-12-10 17:26:40 +0100

    update address email

--- robot/ros_ws/src/herminebot_head/setup.py
+++ robot/ros_ws/src/herminebot_head/setup.py
@@ -20,7 +20,7 @@ setup(
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='axel',
-    maintainer_email='77966063+Axel927@users.noreply.github.com',
+    maintainer_email='axelt.hrc@gmail.com',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
